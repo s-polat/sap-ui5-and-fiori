2ab4a9c1cf75c8d89e9ee0f43a621c83d7bbd915
s-polat <spolatde@gmail.com>
2022-08-26 20:35:47 +0200

    ui5 card text and card header created

--- ui5WebComponentForReact/my-app/src/MyApp.jsx
+++ ui5WebComponentForReact/my-app/src/MyApp.jsx
@@ -1,5 +1,27 @@
-import React from 'react';
+import { Card, CardHeader, Text } from "@ui5/webcomponents-react";
+import React from "react";
+import { spacing } from "@ui5/webcomponents-react-base";
 
 export function MyApp() {
-  return <div>My root component</div>;
+  const handleHeaderClick = () => {
+    alert("Header clicked");
+  };
+  return (
+    <div>
+      <Card
+        header={
+          <CardHeader
+            titleText="Card"
+            interactive
+            onClick={handleHeaderClick}
+          />
+        }
+        style={{ width: "300px" }}
+      >
+        <Text style={spacing.sapUiContentPadding}>
+          This is the content area of the Card
+        </Text>
+      </Card>
+    </div>
+  );
 }
