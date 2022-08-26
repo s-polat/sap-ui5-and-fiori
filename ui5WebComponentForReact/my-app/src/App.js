ee990c1561591e5a34db72efc8c9738a09dc6e91
s-polat <spolatde@gmail.com>
2022-08-26 20:02:49 +0200

    first commit

---
+++ ui5WebComponentForReact/my-app/src/App.js
@@ -0,0 +1,19 @@
+import {
+  FlexBox,
+  FlexBoxAlignItems,
+  FlexBoxDirection,
+  FlexBoxJustifyContent,
+  Link,
+  LinkDesign,
+  ShellBar
+} from '@ui5/webcomponents-react';
+
+function App() {
+  return (
+    <>
+      
+    </>
+  );
+}
+
+export default App;
---
+++ ui5WebComponentForReact/my-app/src/MyApp.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export function MyApp() {
+  return <div>My root component</div>;
+}
